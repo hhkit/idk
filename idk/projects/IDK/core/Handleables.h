01773fb499a0f940658f2f0aa81fd89152b73546
forcasualplayers <38537881+forcasualplayers@users.noreply.github.com>
2019-08-04 14:48:23 +0800

    updated handleable list

--- idk/projects/IDK/core/Handleables.h
+++ idk/projects/IDK/core/Handleables.h
@@ -1,32 +1,40 @@
 #pragma once
 #include <idk.h>
+#include <tuple>
 
 namespace idk
 {
 	/// NEVER CHANGE THIS TUPLE WITHOUT ASKING THE TECH LEAD
 	/// YOU WILL BREAK ALL SERIALIZATION
-	using Handleables = tuple<
-		class GameObject,
-		class Transform,
-		class Parent
+	using Components = tuple<
+		class Transform
+	,	class Parent
 
 	/// EDITOR
-	// class Prefab,
+	//, class Prefab
 
 	/// PHYSICS
-	//	class RigidBody,
-	//	class Collider,
+	//,	class RigidBody
+	//,	class Collider
+	//, class Constraint
 
 	/// GRAPHICS
-	//	class MeshRenderer,
+	//,	class MeshRenderer
+	//, class Light
 
 	/// ANIMATION
-	//	class Animator,
+	//,	class Animator
 
 	/// SCRIPTING
-	//	class MonoBehavior,
+	//,	class MonoBehavior
 
 	/// AUDIO
-	//	class AudioSource
+	//,	class AudioSource
+	//, class AudioListener
 	>;
+
+	using Handleables = std::decay_t<decltype(std::tuple_cat(
+		std::declval<std::tuple<class GameObject>>(),
+		std::declval<Components>()
+	))>;
 }
