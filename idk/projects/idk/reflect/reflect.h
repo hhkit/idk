6269b234e0923f810ca7548a9d2e97b40a49d133
Malody Hoe <malodyhoe@gmail.com>
2019-08-17 00:06:34 +0800

    remove commented macros in reflect.h, place some files in appropriate filters

--- idk/projects/idk/reflect/reflect.h
+++ idk/projects/idk/reflect/reflect.h
@@ -197,17 +197,6 @@ namespace idk::reflect
 
 
 
-//#define REFLECT_VAR_FNBEGIN(NAME, TYPE)		property_var_fnbegin(NAME, TYPE)
-//#define REFLECT_VAR_FNEND()					property_var_fnend()
-//#define REFLECT_LIST_FNBEGIN(NAME, TYPE)		property_list_fnbegin(NAME, TYPE)
-//#define REFLECT_LIST_FNENUM()					property_list_fnenum()
-//#define REFLECT_LIST_FNEND()					property_list_fnend()
-//#define REFLECT_SCOPE_BEGIN(NAME)				property_scope_begin(NAME)
-//#define REFLECT_SCOPE_END()					property_scope_end()
-//#define REFLECT_BEGIN_ALIAS(CLASS_TYPE, ALIAS)	template<> struct property::opin::def<CLASS_TYPE>{ using t_self = CLASS_TYPE; constexpr static char m_Name[] = ALIAS; inline static const idk::reflect::detail::table_storage m_Storage { idk::reflect::detail::class_holder<t_self>{},
-//#define REFLECT_BEGIN(CLASS_TYPE)				REFLECT_BEGIN_ALIAS(CLASS_TYPE, #CLASS_TYPE)
-
-//
 #define REFLECT_BEGIN(CLASS_TYPE, ALIAS)		template<> struct property::opin::def<CLASS_TYPE>{ using t_self = CLASS_TYPE; constexpr static char m_Name[] = ALIAS; inline static const idk::reflect::detail::table_storage m_Storage { idk::reflect::detail::class_holder<t_self>{},
 #define REFLECT_END()							}; inline static const property::table_hash<m_Storage.entry_count_v> m_Table{ m_Storage, m_Name }; inline static const idk::reflect::detail::register_type<t_self, true> __reg{}; };
 #define REFLECT_VAR(VAR)						property::PropertyVar<decltype(std::declval<t_self>().VAR)>( #VAR, offsetof( t_self, VAR ) )
