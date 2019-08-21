07995d12bf3243c693e1473d925d9c98038e244e
Wei Xiang <weixiang.c@digipen.edu>
2019-08-21 14:35:07 +0800

    Revert "Revert "We have debug draw now. Just need to make sure that the shapes are rendered correctly.""
    This reverts commit 25cb674df3861a665204b52f08a8316fe69e29b8.

--- idk/projects/idk/core/Core.cpp
+++ idk/projects/idk/core/Core.cpp
@@ -67,12 +67,18 @@ namespace idk
 				auto &dbg_renderer = Core::GetSystem<DebugRenderer>();
 				if (&dbg_renderer)
 				{
-					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.3f,0,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 3 }, vec4{ 0,1,0,1 });
-					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.2f,0.4f,0 }, vec3{ 0.2f,0.2f,1 }, vec3{ 0,0,1 }, rad{ pi / 6 }, vec4{ 0,1,0,1 });
-					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.1f,0,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 2 }, vec4{ 0,1,0,1 });
-					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.3f,-0.3f,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 5 }, vec4{ 0,1,0,1 });
-					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ -0.3f,0.2f,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 4 }, vec4{ 0,1,0,1 });
-					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ -0.3f,0,0 }, vec3{ 1,0.1f,1 }, vec3{ 0,0,1 }, rad{ pi/30 }, vec4{ 0,1,0,1 });
+					int max_count = 10;
+					for (int i = 0; i < max_count; ++i)
+					{
+						float chunk = 2.0f / max_count;
+						dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ -1.0f+chunk*i,0,0 }, vec3{ chunk,chunk,chunk}, vec3{ 0,0,1 }, rad{ pi *chunk*i }, vec4{ 0,1,0,1 });
+
+					}
+					//dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.2f,0.4f,0 }, vec3{ 0.2f,0.2f,1 }, vec3{ 0,0,1 }, rad{ pi / 6 }, vec4{ 0,1,0,1 });
+					//dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.1f,0,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 2 }, vec4{ 0,1,0,1 });
+					//dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ 0.3f,-0.3f,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 5 }, vec4{ 0,1,0,1 });
+					//dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ -0.3f,0.2f,0 }, vec3{ 1,1,1 }, vec3{ 0,0,1 }, rad{ pi / 4 }, vec4{ 0,1,0,1 });
+					dbg_renderer.DrawShape(DbgShape::eSquare, vec3{ -0.3f,0,0 }, vec3{ 1.0f,0.5f,1.0f }, vec3{ 0,0,1 }, rad{ pi/3 }, vec4{ 0,1,0,1 });
 					dbg_renderer.Render();
 				}
 				//Core::GetSystem<GraphicsSystem>().EndFrame();
