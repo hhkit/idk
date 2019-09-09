18c85b2efdfa6f167e02440e85327bb31db0c685
Malody Hoe <malodyhoe@gmail.com>
2019-09-09 16:05:19 +0800

    impl prefab apply & test, does not handle added/removed components yet

--- idk/projects/idk/prefab/PrefabData.cpp
+++ idk/projects/idk/prefab/PrefabData.cpp
@@ -18,4 +18,24 @@ namespace idk
         }
         return reflect::dynamic();
     }
+
+	int PrefabData::GetComponentIndex(string_view name, int index) const
+	{
+		int i = 0;
+		auto comp_type = reflect::get_type(name);
+		for (auto& original_comp : components)
+		{
+			if (original_comp.type == comp_type)
+			{
+				if (index == 0)
+					return i;
+				else
+					--index;
+			}
+
+			++i;
+		}
+
+		return -1;
+	}
 }
