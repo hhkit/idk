5c4ee211a1dcde8646e3418eb9df5631453ffe64
Joseph Cheng <chengjosephjy@gmail.com>
2019-09-16 16:41:43 +0800

    vulkan mesh loader. opengl bug where we doesnt bind buffer fixed. added obj extension.

--- idk/projects/editor/IDE.cpp
+++ idk/projects/editor/IDE.cpp
@@ -46,11 +46,13 @@ namespace idk
 		case GraphicsAPI::OpenGL:
 			_interface = std::make_unique<edt::OI_Interface>(&Core::GetSystem<ogl::Win32GraphicsSystem>().Instance());
 			Core::GetResourceManager().RegisterExtensionLoader<OpenGLFBXLoader>(".fbx");
+			Core::GetResourceManager().RegisterExtensionLoader<OpenGLFBXLoader>(".obj");
 			Core::GetResourceManager().RegisterExtensionLoader<OpenGLFBXLoader>(".md5mesh");
 			break;
 		case GraphicsAPI::Vulkan:
 			_interface = std::make_unique<edt::VI_Interface>(&Core::GetSystem<vkn::VulkanWin32GraphicsSystem>().Instance());
 			Core::GetResourceManager().RegisterExtensionLoader<VulkanFBXLoader>(".fbx");
+			Core::GetResourceManager().RegisterExtensionLoader<VulkanFBXLoader>(".obj");
 			Core::GetResourceManager().RegisterExtensionLoader<VulkanFBXLoader>(".md5mesh");
 			break;
 		default:
