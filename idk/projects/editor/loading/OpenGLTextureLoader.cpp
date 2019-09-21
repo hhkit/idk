fa26caa71cf64da537621fc6b0484d820f301e7d
Ho Han Kit Ivan <1700237@sit.singaporetech.edu.sg>
2019-09-22 01:56:00 +0800

    NO DON'T RESET THE META

--- idk/projects/editor/loading/OpenGLTextureLoader.cpp
+++ idk/projects/editor/loading/OpenGLTextureLoader.cpp
@@ -47,11 +47,40 @@ namespace idk
 	}
 
 	FileResources OpenGLTextureLoader::Create(PathHandle path_to_resource, const MetaFile& path_to_meta)
-	{
-		UNREFERENCED_PARAMETER(path_to_resource);
-		UNREFERENCED_PARAMETER(path_to_meta);
+	{//Assert for now
+		assert(Core::GetSystem<GraphicsSystem>().GetAPI() == GraphicsAPI::OpenGL);
 
-		return Create(path_to_resource);
+		FileResources retval;
+
+		auto texture_handle = Core::GetResourceManager().Emplace<ogl::OpenGLTexture>(path_to_meta.guids[0]);
+
+		auto& first_meta = path_to_meta.resource_metas[0];
+		if (first_meta.is<Texture::Metadata>())
+			texture_handle->SetMeta(first_meta.get<Texture::Metadata>());
+
+		ivec2 size{};
+		int channels{};
+
+		auto data = stbi_load(path_to_resource.GetFullPath().data(), &size.x, &size.y, &channels, 0);
+
+		if (data) // stbi image can fail
+		{
+			auto col_format = [&]() -> InputChannels
+			{	switch (channels)
+			{
+			default:
+			case 1: return InputChannels::RED;
+			case 2: return InputChannels::RG;
+			case 3: return InputChannels::RGB;
+			case 4: return InputChannels::RGBA;
+			}
+			}();
+
+			texture_handle.as<ogl::OpenGLTexture>().Buffer(data, size, col_format);
+
+			retval.resources.emplace_back(RscHandle<Texture>{texture_handle});
+		}
+		return retval;
 	}
 
 };
