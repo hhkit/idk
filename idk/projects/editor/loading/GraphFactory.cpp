934d86bceca04d2f19135962932785d380bc5c00
Malody Hoe <malodyhoe@gmail.com>
2019-09-28 23:52:42 +0800

    can create new material

--- idk/projects/editor/loading/GraphFactory.cpp
+++ idk/projects/editor/loading/GraphFactory.cpp
@@ -2,6 +2,7 @@
 #include "GraphFactory.h"
 #include <res/MetaBundle.h>
 #include <gfx/ShaderProgram.h>
+#include <gfx/ShaderGraph_helpers.h>
 #include <util/ioutils.h>
 
 namespace idk
@@ -12,10 +13,23 @@ namespace idk
 	}
 	unique_ptr<Material> GraphFactory::Create()
 	{
-		auto retval = std::make_unique<shadergraph::Graph>();
-		retval->_shader_program = Core::GetResourceManager().Create<ShaderProgram>();
-        auto meta = retval->GetMeta();
-		return retval;
+		auto graph = std::make_unique<shadergraph::Graph>();
+
+        shadergraph::Node node;
+        node.name = "master\\PBR";
+        node.guid = Guid::Make();
+        node.position = vec2{ 500.0f, 200.0f };
+
+        auto sig = shadergraph::NodeTemplate::GetTable().at(node.name).signatures[0];
+        for (auto in : sig.ins)
+            node.input_slots.push_back({ in, shadergraph::helpers::default_value(in) });
+        for (auto out : sig.outs)
+            node.output_slots.push_back({ out });
+
+        graph->master_node = node.guid;
+        graph->nodes.emplace(node.guid, std::move(node));
+		graph->_shader_program = Core::GetResourceManager().Create<ShaderProgram>();
+		return graph;
 	}
 	ResourceBundle GraphLoader::LoadFile(PathHandle p)
 	{
