88fbecf569730c5cfef404c9db26aba109579692
ivan.ho <ivan.ho@DIT0282SG>
2019-10-01 15:09:54 +0800

    added C# wrapper library, now to actually bind it

---
+++ idk/wrapper/Time.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idk
+{
+    public class Time
+    {
+        /// <summary>
+        ///     Time of the frame in seconds
+        /// </summary>
+        public static float deltaTime
+        {
+            get { return 0; }
+        }
+
+        public static float fixedDeltaTime
+        {
+            get { return 0; }
+        }
+        /// <summary>
+        ///     Time elapsed since program start in seconds
+        /// </summary>
+        public static float elapsedTime
+        {
+            get { return 0; }
+        }
+
+        public static float timeScale
+        {
+            get { return 0; }
+        }
+       
+        public static float unscaledTime
+        {
+            get { return 0; }
+        }
+    }
+}
