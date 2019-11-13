44188f5d3e1a4810f06eaefaf6e0ba2f049f14bd
IZUHARA <izha95@hotmail.com>
2019-11-14 01:40:12 +0800

    [Fix] Capsule-Capsule collisions due to improper closest points

--- idk/projects/idk/phys/collision.cpp
+++ idk/projects/idk/phys/collision.cpp
@@ -20,6 +20,96 @@ namespace idk::phys
 
 	}
 
+	//Given two lines defined by two points each, return the closest pair on both lines. pair.first will return the point on lineA and pair.second returns the point on lineB
+	std::pair<vec3,vec3> ClosestPairPointsLineSegmentToLineSegment( const vec3& lineA_1, const vec3& lineA_2, const vec3& lineB_1, const vec3& lineB_2) {
+		//ClosestPoint at first =
+		const vec3 vectorA		= lineA_2 - lineA_1;
+		const vec3 vectorB		= lineB_2 - lineB_1;
+		const vec3 vectorA1B1	= lineA_1 - lineB_1;
+
+		const float sq_lenA = vectorA.length_sq();  //Always more than 0
+		const float sq_lenB = vectorB.length_sq();  //Always more than 0
+		const float b = vectorA.dot(vectorB);
+		const float d = vectorA.dot(vectorA1B1);
+		const float e = vectorB.dot(vectorA1B1);
+
+
+		const float SMALL_NUM = 0.00000001f;		//Avoid division overflow
+		const float D = sq_lenA * sq_lenB - b * b;  // always >= 0
+		float ratioA{};								//Clamps to 0.0f to 1.0f
+		float ratioB{};								//Clamps to 0.0f to 1.0f
+		float rangeA{};				// ratioA = rangeA / divideA 
+		float rangeB{};				// ratioB = rangeB / divideB 
+		float divideA = D;			// Starts at D
+		float divideB = D;			// Starts at D
+		
+		if (D < SMALL_NUM)		// If line is parallel (or closely parallel)
+		{ 
+			rangeA = 0.0f;			// force using point P0 on segment S1
+			divideA = 1.0f;			// Avoids zero division
+			rangeB = e;
+			divideB = sq_lenB;
+		}
+		else
+		{                 
+			// Finds the range for both lines. This is unclamped where the two points may lie outside of both lines. 
+			rangeA = (b * e - sq_lenB * d);
+			rangeB = (sq_lenA * e - b * d);
+
+
+			
+			if (rangeA < 0.0f)			// Min point on vectorA
+			{        
+				rangeA = 0.0f;
+				rangeB = e;
+				divideB = sq_lenB;
+			}
+			else if (rangeA > divideA)	// Max point on vectorA
+			{  
+				rangeA = divideA;
+				rangeB = e + b;
+				divideB = sq_lenB;
+			}
+		}
+
+		if (rangeB < 0.0f)				// Min point on vectorB
+		{            
+			rangeB = 0.0f;
+			
+			if (-d < 0.0f)				//Re-edit rangeA to find new point
+				rangeA = 0.0f;
+			else if (-d > sq_lenA)
+				rangeA = divideA;
+			else
+			{
+				rangeA = -d;
+				divideA = sq_lenA;
+			}
+		}
+		else if (rangeB > divideB)		// Max point on vectorA
+		{      
+			rangeB = divideB;
+			
+			if ((-d + b) < 0.0f)		//Re-edit rangeA to find new point
+				rangeA = 0.0f;
+			else if ((-d + b) > sq_lenA)
+				rangeA = divideA;
+			else
+			{
+				rangeA = (-d + b);
+				divideA = sq_lenA;
+			}
+		}
+		// finally do the division to get sc and tc
+		ratioA = (abs(rangeA) < SMALL_NUM ? 0.0f : rangeA / divideA);
+		ratioB = (abs(rangeB) < SMALL_NUM ? 0.0f : rangeB / divideB);
+
+		//http://geomalgorithms.com/a07-_distance.html#dist3D_Segment_to_Segment
+
+		return std::pair<vec3, vec3>(lineA_1 + (ratioA * vectorA), lineB_1 + (ratioB * vectorB));
+	}
+
+
 
 	//A + B. Could be greatly optimized with std::hash<vec2> + unordered_set and templated
 	std::vector<vec3> MinkowskiAddition(const std::vector<vec3> set_a, const std::vector<vec3> set_b) {
--- idk/projects/idk/phys/collision_detection/collision_capsule.cpp
+++ idk/projects/idk/phys/collision_detection/collision_capsule.cpp
@@ -15,6 +15,7 @@ Collisions with capsule. Currently has debug renders
 #include "stdafx.h"
 #include "collision_capsule.h"
 #include "collision_sphere.h"
+#include <phys/collision.cpp>
 #include <gfx/DebugRenderer.h> //Temp
 
 namespace idk::phys
@@ -27,101 +28,11 @@ namespace idk::phys
 		const auto line_points_A = lhs.get_line_points(); //A1,A0
 		const auto line_points_B = rhs.get_line_points(); //B1,B0
 
-		const vec3 A_vector = line_points_A.first - line_points_A.second;
-		const vec3 B_vector = line_points_B.first - line_points_B.second;
-		const vec3 A_unit_vector = A_vector.get_normalized();
-		const vec3 B_unit_vector = B_vector.get_normalized();
-		const vec3 cross_vector = A_unit_vector.cross(B_unit_vector);
-		const auto denominator = cross_vector.length_sq();
+		auto closestPairPts = ClosestPairPointsLineSegmentToLineSegment(line_points_A.first, line_points_A.second, line_points_B.first, line_points_B.second);
 
-		vec3 p_A{};
-		vec3 p_B{};
-		if (epsilon_equal(denominator, 0)) { //Check if it is parallel, if it is, then check if it is above or below
-			const auto dot_1 = A_unit_vector.dot(line_points_B.second - line_points_A.second);
-			const auto dot_2 = A_unit_vector.dot(line_points_B.first  - line_points_A.second);
-
-
-			if (dot_1 <= 0 && dot_2 <= 0) {
-				if (abs(dot_1) < abs(dot_2)) { 
-					p_A = line_points_A.second;
-					p_B = line_points_B.second;
-				}
-				else {
-					p_A = line_points_A.second;
-					p_B = line_points_B.first;
-				}
-			}
-			else if (dot_1 >= A_vector.length() && dot_2 >= A_vector.length()) {
-				if (abs(dot_1) < abs(dot_2)) {
-					p_A = line_points_A.first;
-					p_B = line_points_B.second;
-				}
-				else {
-					p_A = line_points_A.first;
-					p_B = line_points_B.first;
-				}
-			}
-			else {	//Parallel and both overlap
-
-				const vec3 center_vector = lhs.center - rhs.center;
-				float length_at_midway = center_vector.dot(rhs.dir) * 0.5f;
-				p_B = rhs.center + rhs.dir * length_at_midway;
-				p_A = lhs.center - rhs.dir * length_at_midway;
-			}
-
-
-		}
-		else {	//Not parallel, they skew TODO FIX! IT DOES NOT WORK
-			const vec3 connecting_vector = line_points_B.second- line_points_A.second;
-			auto determinant_A = mat3{ connecting_vector ,B_unit_vector ,cross_vector }.determinant();
-			auto determinant_B = mat3{ connecting_vector ,A_unit_vector ,cross_vector }.determinant();
-
-			auto projected_length_A = determinant_A / denominator;
-			auto projected_length_B = determinant_B / denominator;
-
-			p_A = line_points_A.second + A_unit_vector * projected_length_A;	//Closest points on 2 infinite lines
-			p_B = line_points_B.second + B_unit_vector * projected_length_B;	//Closest points on 2 infinite lines
-
-			//Clamp to its edges
-			if (projected_length_A < 0)
-				p_A = line_points_A.second;
-			else if (projected_length_A > A_vector.length())
-				p_A = line_points_A.first;
-
-			if (projected_length_B < 0)
-				p_B = line_points_B.second;
-			else if (projected_length_B > B_vector.length())
-				p_B = line_points_B.first;
-
-
-			float final_length{};
-			if (projected_length_A < 0 || projected_length_A > A_vector.length())
-			{
-				final_length = dot(B_unit_vector, (p_A - line_points_B.second));
-				if (final_length < 0)
-					final_length = 0;
-				else if (final_length > B_vector.length())
-					final_length = B_vector.length();
-				p_B = line_points_B.first + (B_unit_vector * final_length);
-			}
-			
-			if (projected_length_B < 0 || projected_length_B > B_vector.length())
-			{
-				final_length = dot(A_unit_vector, (p_B - line_points_A.second));
-				if (final_length < 0)
-					final_length = 0;
-				else if (final_length > A_vector.length())
-					final_length = A_vector.length();
-				p_A = line_points_A.first + (A_unit_vector * final_length);
-			}
-
-			
-
-		}
-
-		Core::GetSystem<DebugRenderer>().Draw(p_A, p_B, color{ 0,0,1,1 });
-		const sphere makeshiftSphereA{ p_A,lhs.radius };
-		const sphere makeshiftSphereB{ p_B,lhs.radius };
+		Core::GetSystem<DebugRenderer>().Draw(closestPairPts.first, closestPairPts.second, color{ 0,0,1,1 });
+		const sphere makeshiftSphereA{ closestPairPts.first,lhs.radius };
+		const sphere makeshiftSphereB{ closestPairPts.second,lhs.radius };
 
 		auto collisionRes = collide_sphere_sphere_discrete(makeshiftSphereA, makeshiftSphereB);
 		if (collisionRes) {
