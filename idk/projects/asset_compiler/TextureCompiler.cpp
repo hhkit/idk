99a1c541dbee00a3625ff8949e181da8fabccf1e
ivan.ho <ivan.ho@DIT0282SG>
2019-11-14 18:54:12 +0800

    so THAT'S why it was dying

--- idk/projects/asset_compiler/TextureCompiler.cpp
+++ idk/projects/asset_compiler/TextureCompiler.cpp
@@ -7,17 +7,21 @@ namespace idk
 {
 	AssetBundle TextureCompiler::LoadAsset(string_view full_path, const MetaBundle& bundle)
 	{
-		auto in_file = '\"' + string{ full_path } +'\"';
-		auto out_file = static_cast<string>(Guid::Make()) +".dds";
+		constexpr auto wrap = [](string_view str) -> string
+		{
+			return '\"' + string{ str} +'\"';
+		};
+
+		auto out_path = string{ full_path } +".dds";
 
 		auto retval = _spawnl(P_WAIT, "tools/nvtt/nvcompress.exe", 
 			"nvcompress.exe", 
-			in_file.data(),
-			out_file.data(),
+			wrap(full_path).data(),
+			wrap(out_path).data(),
 			0);
 
 		if (retval == 0)
-			return DDSCompiler::LoadAsset(out_file, bundle);
+			return DDSCompiler::LoadAsset(out_path, bundle);
 
 		return AssetBundle{};
 	}
