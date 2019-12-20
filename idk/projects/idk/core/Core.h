7d613af69d099a2a2fbe2a5f6e018338ea58f575
t.hengchee <t.hengchee@DIT0691SG>
2019-12-20 22:50:25 +0800

    PImpled Core to get rid of some dependency issues. Doesn't eliminate all the other dependency issues though.

--- idk/projects/idk/core/Core.h
+++ idk/projects/idk/core/Core.h
@@ -1,9 +1,9 @@
 #pragma once
-#include <core/GameState.h>
 #include <core/SystemManager.h>
 
 namespace idk
 {
+	class GameState;
 	class Scheduler;
 	class ResourceManager;
 	namespace mt { class ThreadPool; }
@@ -15,7 +15,7 @@ namespace idk
 		template<typename T, typename ... Args> T& AddSystem(Args&& ... args);
 		static seconds GetDT();
 		static seconds GetRealDT();
-		static GameState& GetGameState() { return _instance->_game_state; }
+		static GameState& GetGameState();
 		static Scheduler& GetScheduler();
 		static mt::ThreadPool& GetThreadPool();
 		static ResourceManager& GetResourceManager();
@@ -29,14 +29,13 @@ namespace idk
 		void Setup();
 		void Run();
 	private:
-		GameState              _game_state;
-		SystemManager          _system_manager;
-		unique_ptr<Scheduler>  _scheduler;
-		unique_ptr<mt::ThreadPool> _thread_pool;
-		atomic<bool>           _running;
-		static inline Core*    _instance = nullptr;
-		bool                   _setup = false;
-		bool                   _shutdown = false;
+		struct PImpl;
+
+		SystemManager& GetSystemManager();
+		const SystemManager& GetSystemManager()const;
+
+		static inline Core* _instance = nullptr;
+		std::unique_ptr<PImpl> _pimpl;
 	};
 }
 #include "Core.inl"
