95af1ac958b88b68745897ebddfbee0574feba76
t.hengchee <t.hengchee@DIT0691SG>
2019-12-30 22:52:35 +0800

    Added a DebugUtil check that was missed out somehow (insert label)

--- idk/projects/vkn/DebugUtil.cpp
+++ idk/projects/vkn/DebugUtil.cpp
@@ -19,20 +19,19 @@ namespace idk::vkn::dbg
 	void AddLabel(vk::CommandBuffer cmd_buffer, const char* label, const color& color)
 	{
 		auto dbg_label = CreateLabel(label, color);
-		cmd_buffer.insertDebugUtilsLabelEXT(dbg_label, View().DynDispatcher());
+		if (View().DynDispatcher().vkQueueInsertDebugUtilsLabelEXT)
+			cmd_buffer.insertDebugUtilsLabelEXT(dbg_label, View().DynDispatcher());
 	}
 
 	void BeginLabel(vk::CommandBuffer cmd_buffer, const char* label, const color& color)
 	{
 		auto dbg_label = CreateLabel(label, color);
-#pragma message("YO HC I ADDED THIS. PLEASE REMOVE IF YOU'RE OKAY WITH THIS LINE.")
 		if (View().DynDispatcher().vkQueueBeginDebugUtilsLabelEXT)
 			cmd_buffer.beginDebugUtilsLabelEXT(dbg_label, View().DynDispatcher());
 	}
 
 	void EndLabel(vk::CommandBuffer cmd_buffer)
 	{
-#pragma message("I ADDED THIS TOO")
 		if (View().DynDispatcher().vkQueueEndDebugUtilsLabelEXT)
 			cmd_buffer.endDebugUtilsLabelEXT(View().DynDispatcher());
 	}
