7bc989f4c09249a71dd1a9811b772bc9b4199ed5
t.hengchee <t.hengchee@DIT0691SG>
2020-01-03 19:46:02 +0800

    const correctness for index_span and size function

--- idk/projects/idk/ds/index_span.h
+++ idk/projects/idk/ds/index_span.h
@@ -6,8 +6,9 @@ namespace idk
 	struct index_span
 	{
 		size_t begin = 0, end = 0;
+		size_t size()const { return end - begin; }
 		template<typename T, typename span_t = span<std::remove_reference_t<decltype(*std::data(std::declval<T>()))>>>
-		span_t to_span(T& container);
+		span_t to_span(T& container)const;
 	};
 
 }
--- idk/projects/idk/ds/index_span.inl
+++ idk/projects/idk/ds/index_span.inl
@@ -3,7 +3,7 @@
 namespace idk
 {
 	template<typename T, typename span_t>
-	span_t index_span::to_span(T& container)
+	span_t index_span::to_span(T& container)const
 	{
 		return span_t{ std::data(container) + begin ,std::data(container) + end };
 	}
