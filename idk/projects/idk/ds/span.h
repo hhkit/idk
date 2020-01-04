72d9e432604253171e0d42e576103da50a882fe2
t.hengchee <t.hengchee@DIT0691SG>
2020-01-04 19:09:47 +0800

    span User-defined Conversion Guide, span<U> to span <T> conversion.

--- idk/projects/idk/ds/span.h
+++ idk/projects/idk/ds/span.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <utility>
-
+#include <iterator>
 namespace idk
 {
 	template <typename T>
@@ -13,9 +13,14 @@ namespace idk
 		constexpr span(T* begin, T* end) noexcept;
 		template <typename Container, typename = std::enable_if_t<!std::is_same_v<Container, span>>>
 		constexpr explicit span(Container& c) noexcept;
+		template<typename U,typename = std::enable_if_t<std::is_convertible_v<U(*)[],T(*)[]>>>
+		span(span<U> other) : span{other._begin,other._end}{}
 		constexpr T* begin() const noexcept;
 		constexpr T* end() const noexcept;
 		constexpr size_t size() const noexcept;
 		constexpr T& operator[](size_t index) const noexcept;
 	};
+	//User defined deduction guide (C++17 onwards)
+	template<typename Container>
+	explicit span(Container&)-> span < typename std::iterator_traits<decltype(std::declval<Container>().data())>::value_type>;
 }
