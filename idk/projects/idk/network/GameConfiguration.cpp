14bcd25ccd59794d8fe4e7ddee2d6354265593bf
ivan.ho <ivan.ho@DIT0282SG>
2020-01-18 18:11:00 +0800

    idk where this bug is

--- idk/projects/idk/network/GameConfiguration.cpp
+++ idk/projects/idk/network/GameConfiguration.cpp
@@ -4,6 +4,7 @@
 namespace idk
 {
 	GameConfiguration::GameConfiguration()
+		: ClientServerConfig{}
 	{
 		numChannels = 2;
 		channel[(int)GameChannel::RELIABLE].type = yojimbo::CHANNEL_TYPE_RELIABLE_ORDERED;
