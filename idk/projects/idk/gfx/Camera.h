e2d61e9167babca25a36cf4edaecff2ff47f63d5
Malody Hoe <malodyhoe@gmail.com>
2020-02-15 17:13:59 +0800

    WindowsApplication mouse delta divide by WHEEL_DELTA macro, remove some unused fns from Camera

--- idk/projects/idk/gfx/Camera.h
+++ idk/projects/idk/gfx/Camera.h
@@ -38,26 +38,10 @@ namespace idk
 		real gamma = 2.2f;
 		void LookAt(vec3 target_point, vec3 up = vec3{ 0, 1, 0 });
 
-		vec3	  currentPosition() const;
-		vec3	  currentDirection() const;
-
-		Frustum   getFrustum() const;
-
-		mat4      getTightProjection() const;
-		vec3      getFarCorner() const;
-		real	  getBSphereRadius() const;
-
-		vec3 getTightProjectionCenter() const;
-
-		mat4	  ViewMatrix() const;
-		mat4	  ProjectionMatrix() const;
-
+		mat4	   ViewMatrix() const;
+		mat4	   ProjectionMatrix() const;
+		Frustum    GetFrustum() const;
+		float      AspectRatio() const;
 		CameraData GenerateCameraData() const;
-		float      AspectRatio()const;
-	private:
-		//Basic controls parameter
-
-		mat4 _viewMatrix;
-		mat4 _projectionMatrix;
 	};
 }
