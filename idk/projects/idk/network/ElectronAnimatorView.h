f29a6ca619722a7b4a27d1cf307562ddf7c2cd0f
Wei Xiang <weixiang.c@digipen.edu>
2020-03-03 15:22:48 +0800

    Some small fix and I still don't get why the lights flicker like mad

--- idk/projects/idk/network/ElectronAnimatorView.h
+++ idk/projects/idk/network/ElectronAnimatorView.h
@@ -1,15 +1,1 @@
 #pragma once
-#pragma once
-#include <core/Component.h>
-#include <network/network.h>
-namespace idk
-{
-	class ElectronAnimatorView
-		: public Component<ElectronAnimatorView>
-	{
-	public:
-		NetworkID GetNetworkID() const;
-		Handle<ElectronView> GetView() const;
-		void Start();
-	};
-}
