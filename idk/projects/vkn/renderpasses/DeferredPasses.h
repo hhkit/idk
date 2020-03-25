467fa7e0958777af840b3b157be86f106b3b39dc
t.hengchee <t.hengchee@DIT0691SG>
2020-03-25 22:28:01 +0800

    Split Bloom Pass out of DeferredPasses

--- idk/projects/vkn/renderpasses/DeferredPasses.h
+++ idk/projects/vkn/renderpasses/DeferredPasses.h
@@ -133,52 +133,6 @@ namespace idk::vkn::renderpasses
 		rect _viewport;
 	};
 
-	struct BloomPass : BaseRenderPass, FsqUtil
-	{
-		FrameGraphResourceMutable bloom_rsc;
-		FrameGraphResourceMutable depth_rsc;
-		FrameGraphResourceMutable brightness_read_only;
-		//FrameGraphResourceMutable bloom_depth_rsc;
-
-		VknTextureView bright_texture;
-		VknTextureView color_correction_lut;
-
-		RscHandle<ShaderProgram> bloom_shader;
-
-		BloomPass(FrameGraphBuilder& builder, FrameGraphResource out_color, FrameGraphResource color, FrameGraphResource depth, FrameGraphResource hdr, FrameGraphResource gViewPos, rect viewport);
-		void Execute(FrameGraphDetail::Context_t context) override;
-		rect _viewport;
-	};
-
-	struct BloomPassH : BaseRenderPass, FsqUtil
-	{
-		FrameGraphResourceMutable bloom_rsc;
-		FrameGraphResourceMutable brightness_read_only;
-		//FrameGraphResourceMutable bloom_depth_rsc;
-
-		VknTextureView bright_texture;
-
-		RscHandle<ShaderProgram> bloom_shader;
-
-		BloomPassH(FrameGraphBuilder& builder, FrameGraphResource out_color, FrameGraphResource color, FrameGraphResource hdr, rect viewport);
-		void Execute(FrameGraphDetail::Context_t context) override;
-		rect _viewport;
-	};
-
-	struct BloomPassW : BaseRenderPass, FsqUtil
-	{
-		FrameGraphResourceMutable bloom_rsc;
-		FrameGraphResourceMutable brightness_read_only;
-		//FrameGraphResourceMutable bloom_depth_rsc;
-
-		VknTextureView bright_texture;
-
-		RscHandle<ShaderProgram> bloom_shader;
-
-		BloomPassW(FrameGraphBuilder& builder, FrameGraphResource out_color, FrameGraphResource color, FrameGraphResource hdr, rect viewport);
-		void Execute(FrameGraphDetail::Context_t context) override;
-		rect _viewport;
-	};
 
 	struct CubeClearPass : DrawSetRenderPass, FsqUtil
 	{
