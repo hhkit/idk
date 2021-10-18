66733960e9a62ed270af75d5e277baf17a1115b3
HungryCheddar <49947646+HungryCheddar@users.noreply.github.com>
2021-10-18 12:19:15 +0800

    added extra error dialog for basic info (what kind of exception crashed the app) (#3)

--- idk/projects/launcher/game_main.cpp
+++ idk/projects/launcher/game_main.cpp
@@ -49,6 +49,8 @@
 
 #include <vkn/ExtraConfigs.h>
 
+bool overrideMinHide = false;
+
 bool HasArg(std::wstring_view arg, LPWSTR* args, int num_args)
 {
 	bool result = false;
@@ -73,7 +75,7 @@ std::optional<std::wstring_view> GetArgValue(std::wstring_view arg, LPWSTR* args
 }
 LRESULT ProcessAltTab(idk::Windows& win)//, HWND hwnd, UINT msg, WPARAM wParam, [[maybe_unused]] LPARAM lParam)
 {
-	if (win.GetFullscreen())
+	if (win.GetFullscreen() && !overrideMinHide)
 	{
 		//bool is_sys_down = msg == WM_SYSKEYDOWN;
 		//bool is_sys_up = msg == WM_SYSKEYUP;
@@ -98,6 +100,27 @@ namespace idk::mt::hack
 	void SetHelperThreadOverride(int num);
 }
 
+//HACK
+HWND _hwnd{};
+
+struct ManagedErrorString
+{
+	ManagedErrorString(DWORD NTStatusMessage, ...);
+	ManagedErrorString(DWORD NTStatusMessage, int count = 0, DWORD_PTR* test = nullptr);
+	ManagedErrorString(const ManagedErrorString&) = delete; 
+
+	~ManagedErrorString()
+	{
+
+		LocalFree(lpMessageBuffer);
+		FreeLibrary(Hand);
+	}
+	LPWSTR lpMessageBuffer{};
+	size_t numChars = 0;
+	HMODULE Hand{};
+};
+void RegisterUnhandledExceptionHandler();
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
@@ -106,10 +129,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	using namespace idk;
 	int num_args = 0;
 	auto command_lines = CommandLineToArgvW(lpCmdLine, &num_args);
-	SetUnhandledExceptionFilter( [](_In_ struct _EXCEPTION_POINTERS* info) -> LONG {
-		Core::GetSystem<LogSystem>().FlushAllLogs();
-		return EXCEPTION_EXECUTE_HANDLER;
-		});
+	//HWND _hwnd{};
+	RegisterUnhandledExceptionHandler();
 
 	if (HasArg(L"--single_thd", command_lines, num_args))
 	{
@@ -118,6 +139,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	auto c = std::make_unique<Core>();
 
 	auto& win = c->AddSystem<Windows>(hInstance, nCmdShow, LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2)));
+	_hwnd = win.GetWindowHandle();
 	idk::hack::LogSystemConfig::GetSingleton().enabled = HasArg(L"--log", command_lines, num_args);
 
 
@@ -260,4 +282,151 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	c->Run();
 	return c->GetSystem<Windows>().GetReturnVal();
+}
+
+#pragma optimize("",off)
+ManagedErrorString::ManagedErrorString(DWORD NTStatusMessage, ...)
+{
+	va_list list;
+	va_start(list, NTStatusMessage);
+	Hand = LoadLibrary(L"NTDLL.DLL");
+
+
+	numChars = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS,
+		Hand,
+		NTStatusMessage,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMessageBuffer,
+		0,
+		&list);
+
+}
+ManagedErrorString::ManagedErrorString(DWORD NTStatusMessage, int count , DWORD_PTR* test )
+{
+	Hand = LoadLibrary(L"NTDLL.DLL");
+
+
+	numChars = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_ARGUMENT_ARRAY | FORMAT_MESSAGE_IGNORE_INSERTS,
+		Hand,
+		NTStatusMessage,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMessageBuffer,
+		count,
+		(va_list*)test);
+}
+#define EXTERNAL_EXCEPTION 0xE06D7363
+std::wstring GetReadFlag(DWORD flag)
+{
+	auto isWrite = flag & 0x1;
+	auto isDEP = flag & 0x8;
+	return std::wstring{ isWrite ? L"written" : L"read" } + std::wstring{ isDEP ? L"(DEP)" : L"" };
+}
+std::wstring AccessViolationDesc(const _EXCEPTION_RECORD& record)
+{
+	ManagedErrorString err{ record.ExceptionCode ,0};
+	auto flag_msg = GetReadFlag(record.ExceptionInformation[0]);
+	if (err.numChars)
+	{
+		size_t estimated_size = err.numChars + 8 + 8 + flag_msg.size();
+		do
+		{
+
+		std::wstring result(estimated_size+1,L' ');
+
+		int numCharsWritten = std::swprintf(result.data(), result.size(), err.lpMessageBuffer, record.ExceptionAddress, record.ExceptionInformation[1], flag_msg.c_str());
+		if (numCharsWritten > 0)
+		{
+			result.resize(numCharsWritten);
+			return result;
+		}
+		else
+		{
+			auto lower_bound = estimated_size;
+			lower_bound = 1;
+			estimated_size = std::max(estimated_size, lower_bound);
+			estimated_size *= 2;
+			continue;
+		}
+		} while (true);
+	}
+	return L"";
+}
+std::wstring GetErrorString(const _EXCEPTION_RECORD& record)
+{
+	std::wstring codeName;
+	std::wstring description;
+	std::string sdescription;
+	switch (record.ExceptionCode)
+	{
+	case EXTERNAL_EXCEPTION:
+		codeName = L"Non-System Exception";
+		sdescription = ((std::exception*)record.ExceptionInformation[1])->what();
+		break;
+		case EXCEPTION_ACCESS_VIOLATION          : codeName = L"EXCEPTION_ACCESS_VIOLATION          "; description = AccessViolationDesc(record); break;
+		case EXCEPTION_DATATYPE_MISALIGNMENT     : codeName = L"EXCEPTION_DATATYPE_MISALIGNMENT     "; break;
+		case EXCEPTION_BREAKPOINT                : codeName = L"EXCEPTION_BREAKPOINT                "; break;
+		case EXCEPTION_SINGLE_STEP               : codeName = L"EXCEPTION_SINGLE_STEP               "; break;
+		case EXCEPTION_ARRAY_BOUNDS_EXCEEDED     : codeName = L"EXCEPTION_ARRAY_BOUNDS_EXCEEDED     "; break;
+		case EXCEPTION_FLT_DENORMAL_OPERAND      : codeName = L"EXCEPTION_FLT_DENORMAL_OPERAND      "; break;
+		case EXCEPTION_FLT_DIVIDE_BY_ZERO        : codeName = L"EXCEPTION_FLT_DIVIDE_BY_ZERO        "; break;
+		case EXCEPTION_FLT_INEXACT_RESULT        : codeName = L"EXCEPTION_FLT_INEXACT_RESULT        "; break;
+		case EXCEPTION_FLT_INVALID_OPERATION     : codeName = L"EXCEPTION_FLT_INVALID_OPERATION     "; break;
+		case EXCEPTION_FLT_OVERFLOW              : codeName = L"EXCEPTION_FLT_OVERFLOW              "; break;
+		case EXCEPTION_FLT_STACK_CHECK           : codeName = L"EXCEPTION_FLT_STACK_CHECK           "; break;
+		case EXCEPTION_FLT_UNDERFLOW             : codeName = L"EXCEPTION_FLT_UNDERFLOW             "; break;
+		case EXCEPTION_INT_DIVIDE_BY_ZERO        : codeName = L"EXCEPTION_INT_DIVIDE_BY_ZERO        "; break;
+		case EXCEPTION_INT_OVERFLOW              : codeName = L"EXCEPTION_INT_OVERFLOW              "; break;
+		case EXCEPTION_PRIV_INSTRUCTION          : codeName = L"EXCEPTION_PRIV_INSTRUCTION          "; break;
+		case EXCEPTION_IN_PAGE_ERROR             : codeName = L"EXCEPTION_IN_PAGE_ERROR             "; break;
+		case EXCEPTION_ILLEGAL_INSTRUCTION       : codeName = L"EXCEPTION_ILLEGAL_INSTRUCTION       "; break;
+		case EXCEPTION_NONCONTINUABLE_EXCEPTION  : codeName = L"EXCEPTION_NONCONTINUABLE_EXCEPTION  "; break;
+		case EXCEPTION_STACK_OVERFLOW            : codeName = L"EXCEPTION_STACK_OVERFLOW            "; break;
+		case EXCEPTION_INVALID_DISPOSITION       : codeName = L"EXCEPTION_INVALID_DISPOSITION       "; break;
+		case EXCEPTION_GUARD_PAGE                : codeName = L"EXCEPTION_GUARD_PAGE                "; break;
+		case EXCEPTION_INVALID_HANDLE            : codeName = L"EXCEPTION_INVALID_HANDLE            "; break;
+		case CONTROL_C_EXIT                      : codeName = L"CONTROL_C_EXIT                      "; break;
+
+	default:
+		codeName = L"Unknown Exception";
+		break;
+	}
+	std::wstringstream stream;
+	stream << std::hex << codeName << L"\nException Code[0x" << record.ExceptionCode << L"] " << L"\nDescription: ";
+	if (description.empty())
+		stream << sdescription.c_str();
+	else
+		stream << description;
+	stream<< L"\nArgument Values: ";
+	for (auto c = record.NumberParameters; c >0; --c)
+	{
+		auto i = record.NumberParameters - c;
+		stream << L"[" << i << L"]: 0x" << std::hex << record.ExceptionInformation[i] << L",";
+	}
+	//stream <<L"\n"<< (const char*)record.ExceptionInformation;
+	return stream.str();
+}
+void RegisterUnhandledExceptionHandler()
+{
+	using namespace idk;
+
+
+	SetUnhandledExceptionFilter([](_In_ struct _EXCEPTION_POINTERS* info) -> LONG {
+
+		Core::GetSystem<LogSystem>().FlushAllLogs();
+		auto errorCode = info->ExceptionRecord->ExceptionCode;
+		auto errStr = ManagedErrorString{ errorCode,2,info->ExceptionRecord->ExceptionInformation[0],info->ExceptionRecord->ExceptionInformation[1] };// , info->ExceptionRecord->NumberParameters, info->ExceptionRecord->ExceptionInformation };
+		overrideMinHide = true;
+		auto str = GetErrorString(*info->ExceptionRecord);
+		MessageBox(_hwnd, str.c_str(), L"Unhandled Exception", MB_OK);
+		auto str2 = GetErrorString(*info->ExceptionRecord);
+		auto errStr2 = ManagedErrorString{ errorCode,2,info->ExceptionRecord->ExceptionInformation[0],info->ExceptionRecord->ExceptionInformation[1] };// , info->ExceptionRecord->NumberParameters, info->ExceptionRecord->ExceptionInformation };
+		auto errStr3 = ManagedErrorString{ errorCode,2,(DWORD_PTR*)info->ExceptionRecord->ExceptionInformation };// , info->ExceptionRecord->NumberParameters, info->ExceptionRecord->ExceptionInformation };
+		return EXCEPTION_EXECUTE_HANDLER;
+		});
 }
